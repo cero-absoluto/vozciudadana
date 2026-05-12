c98c700be584304d7bcc0895960335e8996b2ae6
cero-absoluto <info@ceroabsoluto.es>
2026-05-12 18:04:37 +0200

    Update App.vue

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -98,7 +98,7 @@ html, body { height: 100%; overflow: hidden; }
     overflow: hidden;
   }
   #home-right {
-    width: 325px;
+    width: 450px;
     flex-shrink: 0;
     display: flex;
     flex-direction: column;

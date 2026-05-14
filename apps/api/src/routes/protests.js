ee36ce1cbdc8fcac0cfcc517768b28bbe372d5f3
cero-absoluto <info@ceroabsoluto.es>
2026-05-14 11:25:38 +0200

    Update protests.js

--- apps/api/src/routes/protests.js
+++ apps/api/src/routes/protests.js
@@ -26,6 +26,7 @@ export default async function protestRoutes(app) {
       .from('protests')
       .select('*')
       .gt('ends_at', new Date().toISOString())
+      .or(`starts_at.gt.${new Date().toISOString()}`)
       .order('heat', { ascending: false })
       .limit(100);
 

7b965297faa9175340c8627f41e150ff46157ef2
cero-absoluto <info@ceroabsoluto.es>
2026-05-16 11:13:50 +0200

    Update App.vue

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -158,7 +158,7 @@ html, body { height: 100%; overflow: hidden; }
     font-size: 16px;
     margin: 16px 24px;
   }
-   #s-detail .detail-map { height: 268px; }
+   #s-detail .detail-map { height: 180px; }
   .detail-body {
     display: flex;
     flex-direction: row;
